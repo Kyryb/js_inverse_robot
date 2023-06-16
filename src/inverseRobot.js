ab9e1f034016e9ddd8d3f72c14857b268ba41d5c
Kyryb <kyrybcode@gmail.com>
2023-06-16 20:11:45 +0300

    Function created

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const fixedRobot = {};
+
+  for (const key in robot) {
+    if (robot[key] in fixedRobot) {
+      return null;
+    }
+
+    fixedRobot[robot[key]] = key;
+  }
+
+  return fixedRobot;
 }
 
 module.exports = inverseRobot;
